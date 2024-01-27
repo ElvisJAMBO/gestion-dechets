eebb822012a47db253033bc594b93bc3d4ae8199
ZEBRA <ZEBRA@DESKTOP-NPQIV49>
2024-01-27 11:18:29 +0200

    commit

---
+++ gestion-des-dechets/src/test/java/com/gestiondechets/GestionDesDechetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondechets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesDechetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
